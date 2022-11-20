ced44329b1a5f94b50714c8c5ee0fdf204f9d6cb
Indianmusics <113574739+Indianmusics@users.noreply.github.com>
2022-11-21 02:58:21 +0530

    Create _init__.py

---
+++ Spotify/_init__.py
@@ -0,0 +1,41 @@
+from Spotify.core.bot import AnonXBot
+from Spotify.core.dir import dirr
+from Spotify.core.git import git
+from Spotify.core.userbot import Userbot
+from Spotify.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AnonXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
